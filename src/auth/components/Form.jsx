3d6302d43016c9879750fc44b5c84395eeb4e2fc
Gonza1es <chelion505@gmail.com>
2022-11-29 21:51:09 +0300

    функциональные изменения по авторизации

--- src/auth/components/Form.jsx
+++ src/auth/components/Form.jsx
@@ -1,9 +1,58 @@
 import {useState} from "react";
+import axios from "axios";
 
 export function Form() {
+    const urlAuth = 'http://localhost:8081/api/auth/'
+    const urlContent = 'http://localhost:8081/api/content/'
     const [headerText, setHeaderText] = useState('Вход');
     const [buttonText, setButtonText] = useState('Войти');
     const [loginState, setLoginState] = useState(true);
+    const [loginValue, setLoginValue] = useState("");
+    const [emailValue, setEmailValue] = useState("");
+    const [passwordValue, setPasswordValue] = useState("");
+    const [errorValue, setErrorValue] = useState("")
+
+    const loginRequest = {
+        username: '',
+        password: ''
+    }
+
+    const regRequest = {
+        username: '',
+        email: '',
+        password: ''
+    }
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+        let response
+        if (loginState) {
+            loginRequest.username = loginValue;
+            loginRequest.password = passwordValue;
+            try {
+                response = await axios.post(urlAuth + 'login', loginRequest);
+                   const authAttribute = await axios.get(urlContent, {
+                        headers: {
+                            Authorization: 'Bearer '+ response.data.token
+                        }
+                    }, response);
+                   alert(authAttribute.data);
+            } catch {
+                alert(response.data.message);
+            }
+        } else {
+            regRequest.username = loginValue;
+            regRequest.email = emailValue;
+            regRequest.password = passwordValue;
+            try {
+                response = axios.post(urlAuth + 'registration', regRequest);
+                alert(response.data.message);
+            } catch {
+                alert(response.data.message);
+            }
+        }
+    }
+
 
     function handleTopBarStateChange() {
         /** Handler for top bar Register|Login switcher **/
@@ -38,17 +87,17 @@ export function Form() {
 
             <form>
                 <ul>
-                    <li><input id="login" placeholder="Логин"/></li>
-                    <li><input id="email" hidden={loginState} placeholder="Email"/></li>
-                    <li><input id="password" placeholder="Пароль" type="password"/></li>
+                    <li><input id="login" placeholder="Логин" onChange={event => setLoginValue(event.target.value)}/></li>
+                    <li><input id="email" hidden={loginState} placeholder="Email" onChange={event => setEmailValue(event.target.value)}/></li>
+                    <li><input id="password" placeholder="Пароль" type="password" onChange={event => setPasswordValue(event.target.value)}/></li>
                 </ul>
             </form>
 
             <ul>
-                <li><button id="login-register" onClick={testFunc}>{buttonText}</button></li>
+                <li><button id="login-register" onClick={submitHandler}>{buttonText}</button></li>
                 <li><button hidden={!loginState} id="forgot-password"><span>Забыли пароль?</span></button></li>
             </ul>
-
+            <h2>{errorValue}</h2>
         </div>
     )
 }
