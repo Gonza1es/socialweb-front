663b5c4981ba0641164d1b232d47fb89e328f873
prytkovm <maksim.prytkov.2002@gmail.com>
2022-12-06 01:35:12 +0300

    Начал добавлять роутинг

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import {Auth} from "./auth/Auth";
 import {Profile} from "./main/Profile";
-import {Header} from "./main/components/Header";
 import {Feed} from "./main/Feed";
-import {PostCreator} from "./main/components/PostCreator";
+import {Route, Routes} from "react-router-dom";
+import React from "react";
+import {FirstTimeLogin} from "./main/FirstTimeLogin";
 
 function App() {
     return (
         <div className="App">
-            <Header/>
-            {/*<Feed/>*/}
-            <Profile/>
+            <Routes>
+                <Route path="/" element={<Auth/>}/>
+                <Route index element={<Auth/>}/>
+                <Route path="my-profile" element={<Profile/>}/>
+                <Route path="feed" element={<Feed/>}/>
+                <Route path="additional-info" element={<FirstTimeLogin/>}/>
+            </Routes>
         </div>
     )
 }
