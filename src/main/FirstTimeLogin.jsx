081c4fffa96a713e4360e437a9460ddf676a20de
prytkovm <maksim.prytkov.2002@gmail.com>
2022-12-06 01:30:29 +0300

    Добавлена страница получения доп. данных после регистрации/во время первого входа

---
+++ src/main/FirstTimeLogin.jsx
@@ -0,0 +1,10 @@
+import './styles/FirstTimeLogin.css';
+import {ProfileEditor} from "./components/ProfileEditor";
+
+export function FirstTimeLogin() {
+    return (
+        <div className="first-time-login-container">
+            <ProfileEditor/>
+        </div>
+    )
+}
