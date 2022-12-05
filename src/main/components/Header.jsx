b7a4f90e432d1231611ed5ba974fc7b873ce2030
prytkovm <maksim.prytkov.2002@gmail.com>
2022-12-06 01:35:50 +0300

    Навигационный бар слегка переработан, добавлен роутинг

--- src/main/components/Header.jsx
+++ src/main/components/Header.jsx
@@ -1,5 +1,6 @@
 import '../styles/Header.css'
 import logo from '../logo.svg'
+import {NavLink} from "react-router-dom";
 
 /**
  * @description Хедер с логотипом и нав. баром
@@ -7,6 +8,29 @@ import logo from '../logo.svg'
  * @constructor
  */
 export function Header() {
+
+    const navLinkStyle = {
+        margin: "auto",
+        outline: "none",
+        textDecoration: "none",
+        color: "#464C54",
+        fontSize: "21px",
+        fontWeight: "600",
+        lineHeight: "25px"
+    };
+
+    const navLinkActiveStyle = {
+        margin: "auto",
+        outline: "none",
+        textDecoration: "none",
+        color: "#6D7783",
+        fontSize: "21px",
+        fontWeight: "600",
+        lineHeight: "25px",
+        textDecorationLine: "underline",
+        textUnderlineOffset: "5px"
+    };
+
     return (
         <div className="header-wrapper">
             <div className="header">
@@ -16,11 +40,55 @@ export function Header() {
                 </div>
 
                 <div className="header-navbar">
-                    <a href="#">Моя страница</a>
-                    <a href="#">Новости</a>
-                    <a href="#">Сообщения</a>
-                    <a href="#">Подписки</a>
-                    <a href="#">Выход</a>
+                    <NavLink
+                        to="/my-profile"
+                        style={({ isActive }) =>
+                            isActive ? navLinkActiveStyle : navLinkStyle
+                        }
+                    >
+                        Моя страница
+                    </NavLink>
+
+                    <NavLink
+                        to="/feed"
+                        style={({ isActive }) =>
+                            isActive ? navLinkActiveStyle : navLinkStyle
+                        }
+                    >
+                        Новости
+                    </NavLink>
+
+                    <NavLink
+                        to="/messages"
+                        style={({ isActive }) =>
+                            isActive ? navLinkActiveStyle : navLinkStyle
+                        }
+                    >
+                        Сообщения
+                    </NavLink>
+
+                    <NavLink
+                        to="/subscribes"
+                        style={({ isActive }) =>
+                            isActive ? navLinkActiveStyle : navLinkStyle
+                        }
+                    >
+                        Подписки
+                    </NavLink>
+
+                    <NavLink
+                        to="/logout"
+                        style={({ isActive }) =>
+                            isActive ? navLinkActiveStyle : navLinkStyle
+                        }
+                    >
+                        Выход
+                    </NavLink>
+                    {/*<a>Моя страница</a>*/}
+                    {/*<a href="#">Новости</a>*/}
+                    {/*<a href="#">Сообщения</a>*/}
+                    {/*<a href="#">Подписки</a>*/}
+                    {/*<a href="#">Выход</a>*/}
                 </div>
             </div>
         </div>
